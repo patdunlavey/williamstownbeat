5071010411c740a720b7b78dfed191767749d199
Pat Dunlavey <patdunlavey@gmail.com>
2011-06-23 10:02:37 -0400

    Core update to 6.22

---
+++ sites/all/themes/ninesixty_wb/google_dfp_sb.js
@@ -0,0 +1,12 @@
+<script type='text/javascript' src='http://partner.googleadservices.com/gampad/google_service.js'>
+</script>
+<script type='text/javascript'>
+GS_googleAddAdSenseService("ca-pub-4170618544340918");
+GS_googleEnableAllServices();
+</script>
+<script type='text/javascript'>
+GA_googleAddSlot("ca-pub-4170618544340918", "Full_Banner-All");
+</script>
+<script type='text/javascript'>
+GA_googleFetchAds();
+</script>
